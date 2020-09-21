47b543dbc178f8d5c96537eca0c1e1c6c2bfd09b
Xavier Pillons <xpillons@microsoft.com>
2020-09-21 18:47:05 +0200

    create inventory from tags

---
+++ experimental/ansible/scripts/create_inventory.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+INVENTORY_FILE=${1-inventory}
+# Iterate thru all files in the hostlists/tags directory and for each file add the following content to the inventory file
+# [file_name]
+# file_content
+#
+
+for file in hostlists/tags/*; do 
+    if [ -f "$file" ]; then 
+        base=$(basename $file)
+        echo "[$base]" >> $INVENTORY_FILE
+        cat $file >> $INVENTORY_FILE
+        echo "" >> $INVENTORY_FILE
+    fi 
+done
+
